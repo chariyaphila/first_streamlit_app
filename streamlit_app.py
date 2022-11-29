ea301f77ae3f2069fb1edb64890c75215d2db800
Chariya PHILA <38211287+chariyaphila@users.noreply.github.com>
2022-11-29 16:19:33 +0100

    Update streamlit_app.py
    Added header and 3 texts.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('Hands on lab Badge 2')
+
+streamlit.header('What will I learn?')
+
+streamlit.text('Step 1 : Setup')
+streamlit.text('Step 2 : GitHub & Streamlit')
+streamlit.text('To be continue')
